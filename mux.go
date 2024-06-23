c47d241c8e52a3361a1f1923ad7e94d0a2376b38
Vassili Baranov <vassili.baranov@gmail.com>
2024-06-23 15:31:09 +0300

    create MessageMux

---
+++ mux.go
@@ -0,0 +1,59 @@
+package ugubroker
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+var (
+	ErrNoHandlerFound = errors.New("no handler found for topic")
+)
+
+type Message struct {
+	Topic string
+	Data  []byte
+}
+
+type MessageHandler interface {
+	ServeMessage(context.Context, Message) error
+}
+
+type MessageMux struct {
+	handlers map[string]MessageHandler
+	mu       sync.RWMutex
+}
+
+func NewMessageMux() *MessageMux {
+	return &MessageMux{
+		handlers: make(map[string]MessageHandler),
+	}
+}
+
+func (mux *MessageMux) Subscribe(topic string, handler MessageHandler) {
+	mux.mu.Lock()
+	defer mux.mu.Unlock()
+
+	mux.handlers[topic] = handler
+}
+
+func (mux *MessageMux) Handler(topic string) MessageHandler {
+	if topic == "" {
+		return nil
+	}
+
+	mux.mu.RLock()
+	defer mux.mu.RUnlock()
+
+	return mux.handlers[topic]
+}
+
+func (mux *MessageMux) ServeMessage(ctx context.Context, m Message) error {
+	h := mux.Handler(m.Topic)
+	if h == nil {
+		return fmt.Errorf("%w: %s", ErrNoHandlerFound, m.Topic)
+	}
+
+	return h.ServeMessage(ctx, m)
+}
